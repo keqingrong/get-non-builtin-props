2a74a43b685f8162eb51eecd6d515ddd44822997
Qingrong Ke <keqingrong1992@gmail.com>
2021-09-12 13:18:41 +0800

    feat: exclude `getNonBuiltinProps` and temp iframe variable from results

--- src/index.js
+++ src/index.js
@@ -2,17 +2,38 @@
  * Get non-builtin properties on the `window` object
  */
 function getNonBuiltinProps() {
-  var nonBuiltinObjects = {};
-  var iframe = document.createElement('iframe');
+  const nonBuiltinObjects = {};
+  const iframe = document.createElement('iframe');
   iframe.style.display = 'none';
   document.body.appendChild(iframe);
-  var builtinPropKeys = Object.keys(iframe.contentWindow);
+
+  const builtinPropKeys = Object.keys(iframe.contentWindow);
 
   Object.keys(window).forEach(function (key) {
     if (!builtinPropKeys.includes(key)) {
       nonBuiltinObjects[key] = window[key];
     }
   });
+
+  const nonBuiltinPropKeys = Object.keys(nonBuiltinObjects);
+  let tempIframeKey;
+  let thisFnKey;
+  if (
+    (tempIframeKey = nonBuiltinPropKeys.find(
+      key => /^\d+$/.test(key) && iframe.contentWindow === window[key]
+    ))
+  ) {
+    delete nonBuiltinObjects[tempIframeKey];
+  }
+
+  if (
+    (thisFnKey = nonBuiltinPropKeys.find(
+      key =>
+        key === getNonBuiltinProps.name && window[key] === getNonBuiltinProps
+    ))
+  ) {
+    delete nonBuiltinObjects[thisFnKey];
+  }
 
   document.body.removeChild(iframe);
 
